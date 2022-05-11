e296c9949aa2a47bed0f19507539b1730afadb29
Yoan Estrada <yoanestrada01@gmail.com>
2022-05-10 22:23:20 -0500

    ADD: getProjectTask implements added project.controller

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -1,4 +1,5 @@
 import { Project } from '../models/Project.js'
+import { Task } from '../models/Task.js'
 
 export const getProjects = async (req, res) => {
   try {
@@ -63,4 +64,13 @@ export const deleteProject = async (req, res) => {
   } catch (error) {
     return res.status(500).json({ msg: error.message })
   }
+}
+
+export const getProjectTasks = async (req, res) => {
+  const { id } = req.params
+  const tasks = await Task.findAll({
+    where: { projectId: id }
+  })
+
+  res.json(tasks)
 }
--- backend/src/routes/projects.routes.js
+++ backend/src/routes/projects.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getProjects, createProject, updateProject, deleteProject, getProject } from '../controllers/project.controller.js'
+import { getProjects, createProject, updateProject, deleteProject, getProject, getProjectTasks } from '../controllers/project.controller.js'
 const router = Router()
 
 
@@ -10,6 +10,7 @@ router.post('/projects', createProject)
 router.put('/projects/:id', updateProject)
 router.delete('/projects/:id', deleteProject)
 
+router.get('/projects/:id/tasks', getProjectTasks)
 
 
 export default router
